65f841f0aec710279296f5edf3b35ea8c830f896
0951692659aS <s6812022610280@email.kmutnb.ac.th>
2025-07-21 15:42:47 +0700

    3

---
+++ comWk05_31.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+printf("a =\t\t0\t.\t6\t1\t7\t%%\n");
+printf("b =\t\t\t2\t5\t%%\n");
+printf("c    =\t1\t9\t.\t0\t0\t0\t%%\n");
+printf("d \t\t\t=\t9\t\t%%\n");
+printf("e =\t1\t2\t4\t.\t5\t5\t5\t%%\n");
+printf("f =\t0\t.\t0\t0\t1\t%%\n");
+return 0;
+}
